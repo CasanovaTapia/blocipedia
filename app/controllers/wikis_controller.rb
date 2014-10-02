629f32a1d2037dcc5fdb6ee2290ccc10d453cb50
Ching Casanova Tapia <ching@casanovatapia.com>
2014-10-02 17:05:00 +0800

    Created wiki resource

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,59 @@
+class WikisController < ApplicationController
+  def index
+    @wikis = Wiki.all
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def create
+    @wiki = current_user.wikis.new(wiki_params)
+
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash[:error] = "Wiki was not saved. Please try again."
+      render :new
+    end
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.update_attributes(wiki_params)
+      flash[:notice] = "Wiki was updated."
+      redirect_to @wiki
+    else
+      flash[:error] = "Wiki was not updated. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy 
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "Wiki was deleted."
+      redirect_to wikis_path
+    else
+      flash[:error] = "Wiki was not deleted. Please try again."
+      render :show
+    end
+  end
+
+  private
+
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :public)
+  end
+end
